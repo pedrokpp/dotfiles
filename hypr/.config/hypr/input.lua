7b0dcaa06eb4dddffe9c11f9dd8622e0c64f587c
pedrokpp <pedrkp@proton.me>
2026-08-24 23:59:54 -0300

    update keyboard international

--- hypr/.config/hypr/input.lua
+++ hypr/.config/hypr/input.lua
@@ -10,7 +10,7 @@ hl.config({
     kb_options = "compose:ralt,shift:both_capslock_cancel,grp:alts_toggle",
 --
 --     -- Use a specific keyboard variant if needed (e.g. intl for international keyboards).
---     kb_variant = "intl",
+    kb_variant = "intl",
 --
 --     -- Change speed of keyboard repeat.
 --     repeat_rate = 40,
